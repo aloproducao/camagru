9122df0661d5ec534f4d89d3410c3a5842db935a
Tanguy GAUVRIT <tgauvrit@e3r12p20.42.fr>
2016-01-22 12:25:44 +0100

    Light cleaning

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
   <div id="banner" class="container index_banner">
     <section>
       <p><strong>Pussycam</strong> is a new tool that let's you take pictures and add that <strong>special someone</strong> to the mix.</p>
+      <p>Check out our <strong><a href="gallery.php">Public Gallery</a></strong> or...</p>
       <a class="button medium" href="login.php">Log In</a>
       <a class="button medium" href="signup.php">Sign Up</a>
     </section>
